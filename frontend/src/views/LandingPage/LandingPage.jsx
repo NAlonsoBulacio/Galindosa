0f2e5c7e8e4ced81aa0bd551f890519eded8b22d
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-02-21 08:40:52 -0300

    App ready for work

---
+++ frontend/src/views/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../../assets/logo.png';
+const LandingPage = () => {
+  return (
+    <div>
+      <img src={logo} alt="logo-galindosa" />
+    </div>
+  )
+}
+
+export default LandingPage
