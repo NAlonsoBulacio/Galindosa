1cd6f35dff2938fc883cbcea49c1e490d3196778
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-02-26 17:27:33 -0300

    slide fixed

--- frontend/src/components/Slide/Slide.jsx
+++ frontend/src/components/Slide/Slide.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import bar from "../../assets/bar.webp";
 const Slide = ({ title }) => {
   return (
-    <div className="w-full h-full flex flex-wrap justify-center items-center gap-y-[10px] lg:gap-y-4">
-      <div className="w-full h-[270px] flex justify-center items-start">
-        <div className="w-[245px] lg:w-[270px] h-[245px] lg:h-[270px] rounded-full overflow-hidden bg-purple-500">
+    <div className="w-full h-full flex flex-wrap justify-center items-start lg:items-center gap-y-[8px] lg:gap-y-4">
+      <div className="w-full h-[235px] lg:h-[270px] flex justify-center items-start">
+        <div className="w-[235px] lg:w-[270px] h-[235px] lg:h-[270px] rounded-full overflow-hidden bg-purple-500">
           <img className="w-full h-full object-cover" src={bar} alt="" />
         </div>
       </div>
