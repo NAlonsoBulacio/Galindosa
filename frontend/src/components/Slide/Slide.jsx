17569dd157411b7b0bec3bf6e1f7a6ea640ebda2
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-02-26 17:14:59 -0300

    change font-size slider

--- frontend/src/components/Slide/Slide.jsx
+++ frontend/src/components/Slide/Slide.jsx
@@ -4,12 +4,12 @@ const Slide = ({ title }) => {
   return (
     <div className="w-full h-full flex flex-wrap justify-center items-center gap-y-[10px] lg:gap-y-4">
       <div className="w-full h-[270px] flex justify-center items-start">
-        <div className="w-[270px] h-[270px] rounded-full overflow-hidden bg-purple-500">
+        <div className="w-[245px] lg:w-[270px] h-[245px] lg:h-[270px] rounded-full overflow-hidden bg-purple-500">
           <img className="w-full h-full object-cover" src={bar} alt="" />
         </div>
       </div>
       <div className="w-full flex justify-center items-start px-16">
-        <h1 className="h-[80px] text-center text-gray-200 font-plus-400 text-xl">
+        <h1 className="h-[80px] text-center text-gray-200 font-plus-400 text-lg lg:text-xl">
           {title}
         </h1>
       </div>
