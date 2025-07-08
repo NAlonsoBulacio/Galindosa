4ff865837fae50f63dbb5d288e565872f687c541
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2025-07-07 23:03:46 -0300

    datalayer added

--- frontend/src/components/SectionCard/SectionCard.jsx
+++ frontend/src/components/SectionCard/SectionCard.jsx
@@ -1,76 +1,78 @@
-"use client"
+"use client";
 
-import { useState, useEffect, useRef } from "react"
-import { IoIosContacts } from "react-icons/io"
-import SampleNextArrow from "../../utils/SampleNextArrow"
-import SamplePrevArrow from "../../utils/SamplePrevArrow"
-import Slider from "react-slick"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
-import { MdFullscreen, MdFullscreenExit } from "react-icons/md"
-import compressImage from "../../utils/compressImage"
-import screenfull from "screenfull"
+import { useState, useEffect, useRef } from "react";
+import { IoIosContacts } from "react-icons/io";
+import SampleNextArrow from "../../utils/SampleNextArrow";
+import SamplePrevArrow from "../../utils/SamplePrevArrow";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { MdFullscreen, MdFullscreenExit } from "react-icons/md";
+import compressImage from "../../utils/compressImage";
+import screenfull from "screenfull";
 
 const SectionCard = ({ section, index }) => {
-  const [sectionProps, setSectionProps] = useState("")
-  const [isFullscreen, setIsFullscreen] = useState(false)
-  const sliderRef = useRef(null)
+  const [sectionProps, setSectionProps] = useState("");
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const sliderRef = useRef(null);
 
   useEffect(() => {
-    setSectionProps(section)
-  }, [section])
+    setSectionProps(section);
+  }, [section]);
 
-  const isIosSafari = /iP(ad|hone|od).+Version\/\d{2}.*Safari/i.test(navigator.userAgent)
+  const isIosSafari = /iP(ad|hone|od).+Version\/\d{2}.*Safari/i.test(
+    navigator.userAgent
+  );
 
   const toggleFullScreen = () => {
     try {
       if (isIosSafari) {
         if (!isFullscreen) {
-          sliderRef.current.style.position = "fixed"
-          sliderRef.current.style.top = "0"
-          sliderRef.current.style.left = "0"
-          sliderRef.current.style.width = "100vw"
-          sliderRef.current.style.height = "100vh"
-          sliderRef.current.style.zIndex = "9999"
-          sliderRef.current.style.backgroundColor = "black"
+          sliderRef.current.style.position = "fixed";
+          sliderRef.current.style.top = "0";
+          sliderRef.current.style.left = "0";
+          sliderRef.current.style.width = "100vw";
+          sliderRef.current.style.height = "100vh";
+          sliderRef.current.style.zIndex = "9999";
+          sliderRef.current.style.backgroundColor = "black";
         } else {
-          sliderRef.current.style.position = ""
-          sliderRef.current.style.top = ""
-          sliderRef.current.style.left = ""
-          sliderRef.current.style.width = ""
-          sliderRef.current.style.height = ""
-          sliderRef.current.style.zIndex = ""
-          sliderRef.current.style.backgroundColor = ""
+          sliderRef.current.style.position = "";
+          sliderRef.current.style.top = "";
+          sliderRef.current.style.left = "";
+          sliderRef.current.style.width = "";
+          sliderRef.current.style.height = "";
+          sliderRef.current.style.zIndex = "";
+          sliderRef.current.style.backgroundColor = "";
         }
-        setIsFullscreen(!isFullscreen)
+        setIsFullscreen(!isFullscreen);
       } else if (screenfull.isEnabled) {
-        screenfull.toggle(sliderRef.current)
-        setIsFullscreen(!isFullscreen)
+        screenfull.toggle(sliderRef.current);
+        setIsFullscreen(!isFullscreen);
       } else {
-        console.warn("Fullscreen mode is not supported on this browser.")
+        console.warn("Fullscreen mode is not supported on this browser.");
       }
     } catch (error) {
-      console.error("Error toggling fullscreen mode:", error)
+      console.error("Error toggling fullscreen mode:", error);
     }
-  }
+  };
 
   useEffect(() => {
     const handleFullscreenChange = () => {
-      setIsFullscreen(screenfull.isFullscreen)
-    }
+      setIsFullscreen(screenfull.isFullscreen);
+    };
 
     if (screenfull.isEnabled) {
-      screenfull.on("change", handleFullscreenChange)
+      screenfull.on("change", handleFullscreenChange);
     }
 
     return () => {
       if (screenfull.isEnabled) {
-        screenfull.off("change", handleFullscreenChange)
+        screenfull.off("change", handleFullscreenChange);
       }
-    }
-  }, [])
+    };
+  }, []);
 
-  const images = section ? section.images : ""
+  const images = section ? section.images : "";
 
   const settings = {
     infinite: true,
@@ -80,7 +82,7 @@ const SectionCard = ({ section, index }) => {
     dots: !isFullscreen,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
-  }
+  };
 
   return (
     <>
@@ -94,17 +96,36 @@ const SectionCard = ({ section, index }) => {
             <h1 className="text-left text-3xl lg:text-4xl poppins-semibold text-gray-800 font-bold">
               {sectionProps.title}
             </h1>
-            <p className="text-sm lg:text-md poppins-light">{sectionProps.text}</p>
+            <p className="text-sm lg:text-md poppins-light">
+              {sectionProps.text}
+            </p>
             <div className="flex flex-wrap justify-start gap-8 mt-4">
               {sectionProps.amenities?.map((amenity, index) => (
-                <div key={index} className="flex flex-col items-center w-[18%] px-2">
+                <div
+                  key={index}
+                  className="flex flex-col items-center w-[18%] px-2"
+                >
                   <img src={amenity.icon} alt={amenity.label} className="" />
-                  <p className="text-xs lg:text-md text-black text-center">{amenity.label}</p>
+                  <p className="text-xs lg:text-md text-black text-center">
+                    {amenity.label}
+                  </p>
                 </div>
               ))}
             </div>
             <a
-               href="https://wa.me/+5493812071244" target="_blank" rel="noreferrer"
+              href="https://wa.me/+5493812071244"
+              target="_blank"
+              rel="noreferrer"
+              onClick={() => {
+                window.dataLayer = window.dataLayer || [];
+                window.dataLayer.push({
+                  event: "contact",
+                  eventCategory: "Interacción",
+                  eventAction: "Click en botón de consulta",
+                  eventLabel: sectionProps?.title || "Sección sin título",
+                  content_type: "contact",
+                });
+              }}
               className="wp-btn bg-[#ccad24] hover:bg-[#a18c2d] text-gray-50 flex justify-center items-center px-4 py-2 rounded-full text-lg gap-x-2"
             >
               Consultá <IoIosContacts className="text-2xl" />
@@ -114,12 +135,20 @@ const SectionCard = ({ section, index }) => {
           <div className={`${isFullscreen ? "w-screen" : "w-full lg:w-2/4"}`}>
             <div
               ref={sliderRef}
-              className={`${isFullscreen ? "fixed inset-0 z-50 bg-gray-900 bg-opacity-75" : "relative"}`}
+              className={`${
+                isFullscreen
+                  ? "fixed inset-0 z-50 bg-gray-900 bg-opacity-75"
+                  : "relative"
+              }`}
             >
               {images.length > 1 ? (
                 <Slider {...settings}>
                   {images.map((image, index) => (
-                    <div key={index} className="px-2 relative" onClick={!isFullscreen ? toggleFullScreen : null}>
+                    <div
+                      key={index}
+                      className="px-2 relative"
+                      onClick={!isFullscreen ? toggleFullScreen : null}
+                    >
                       <img
                         src={compressImage(image)}
                         alt={`Slide ${index}`}
@@ -135,9 +164,15 @@ const SectionCard = ({ section, index }) => {
                           className="px-3 w-auto h-auto bg-gray-500 bg-opacity-30 hover:bg-opacity-75 rounded-lg opacity-100 duration-150"
                         >
                           {isFullscreen ? (
-                            <MdFullscreenExit width={45} className="text-white w-[30px] text-4xl" />
+                            <MdFullscreenExit
+                              width={45}
+                              className="text-white w-[30px] text-4xl"
+                            />
                           ) : (
-                            <MdFullscreen width={45} className="text-white w-[30px] text-4xl" />
+                            <MdFullscreen
+                              width={45}
+                              className="text-white w-[30px] text-4xl"
+                            />
                           )}
                         </button>
                       </div>
@@ -159,9 +194,15 @@ const SectionCard = ({ section, index }) => {
                         className="px-3 w-auto h-auto bg-gray-500 bg-opacity-30 hover:bg-opacity-75 rounded-lg opacity-100 duration-150"
                       >
                         {isFullscreen ? (
-                          <MdFullscreenExit width={45} className="text-white w-[30px] text-4xl" />
+                          <MdFullscreenExit
+                            width={45}
+                            className="text-white w-[30px] text-4xl"
+                          />
                         ) : (
-                          <MdFullscreen width={45} className="text-white w-[30px] text-4xl" />
+                          <MdFullscreen
+                            width={45}
+                            className="text-white w-[30px] text-4xl"
+                          />
                         )}
                       </button>
                     </div>
@@ -173,8 +214,7 @@ const SectionCard = ({ section, index }) => {
         </div>
       ) : null}
     </>
-  )
-}
+  );
+};
 
-export default SectionCard
-
+export default SectionCard;
--- frontend/src/components/Whatsapp/WhatsappButton.jsx
+++ frontend/src/components/Whatsapp/WhatsappButton.jsx
@@ -2,7 +2,16 @@ import { FaWhatsapp } from "react-icons/fa"
 
 const WhatsAppButton = () => {
   return (
-    <a href="https://wa.me/+5493812071244" target="_blank" rel="noreferrer" className="wp-btn">
+    <a href="https://wa.me/+5493812071244" target="_blank" rel="noreferrer" className="wp-btn" onClick={() => {
+                window.dataLayer = window.dataLayer || [];
+                window.dataLayer.push({
+                  event: "contact",
+                  eventCategory: "Interacción",
+                  eventAction: "Click en botón de consulta",
+                  eventLabel: sectionProps?.title || "Sección sin título",
+                  content_type: "contact",
+                });
+              }}>
       <button
         className="fixed bottom-4 right-4 bg-green-500 hover:bg-green-600 rounded-full h-32 w-32 flex justify-center items-center text-white z-10"
         style={{ height: "50px", width: "50px" }}
