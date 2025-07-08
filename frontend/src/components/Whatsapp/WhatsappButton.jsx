8430b5f2db6e9cd7daeaef15f55a81fe9b07ce92
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2025-07-07 23:04:49 -0300

    galindo sa

--- frontend/src/components/Whatsapp/WhatsappButton.jsx
+++ frontend/src/components/Whatsapp/WhatsappButton.jsx
@@ -1,17 +1,22 @@
-import { FaWhatsapp } from "react-icons/fa"
+import { FaWhatsapp } from "react-icons/fa";
 
 const WhatsAppButton = () => {
   return (
-    <a href="https://wa.me/+5493812071244" target="_blank" rel="noreferrer" className="wp-btn" onClick={() => {
-                window.dataLayer = window.dataLayer || [];
-                window.dataLayer.push({
-                  event: "contact",
-                  eventCategory: "Interacción",
-                  eventAction: "Click en botón de consulta",
-                  eventLabel: sectionProps?.title || "Sección sin título",
-                  content_type: "contact",
-                });
-              }}>
+    <a
+      href="https://wa.me/+5493812071244"
+      target="_blank"
+      rel="noreferrer"
+      className="wp-btn"
+      onClick={() => {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+          event: "contact",
+          eventCategory: "Interacción",
+          eventAction: "Click en botón de consulta",
+          content_type: "contact",
+        });
+      }}
+    >
       <button
         className="fixed bottom-4 right-4 bg-green-500 hover:bg-green-600 rounded-full h-32 w-32 flex justify-center items-center text-white z-10"
         style={{ height: "50px", width: "50px" }}
@@ -19,8 +24,7 @@ const WhatsAppButton = () => {
         <FaWhatsapp size={35} />
       </button>
     </a>
-  )
-}
+  );
+};
 
-export default WhatsAppButton
-
+export default WhatsAppButton;
